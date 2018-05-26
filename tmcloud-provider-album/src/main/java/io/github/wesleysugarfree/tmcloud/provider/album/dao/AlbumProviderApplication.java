887db09bdb43107629e40379bbfa8103fa37ab32
jian.tan <jian.tan@daocloud.io>
2018-05-27 00:23:01 +0800

    update basic structure.

--- tmcloud-provider-album/src/main/java/io/github/wesleysugarfree/tmcloud/provider/album/dao/AlbumProviderApplication.java
+++ tmcloud-provider-album/src/main/java/io/github/wesleysugarfree/tmcloud/provider/album/dao/AlbumProviderApplication.java
@@ -1,14 +1,13 @@
 
 
 package io.github.wesleysugarfree.tmcloud.provider.album.dao;
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = "io.github.wesleysugarfree.tmcloud")
 @EnableEurekaClient
-@MapperScan("io.github.wesleysugarfree.tmcloud.provider.album.dao.mapper")
 public class AlbumProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(AlbumProviderApplication.class, args);
